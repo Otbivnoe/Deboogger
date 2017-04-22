9050acea9f05013598d28f97de0bd3856b9e7ea0
Nikita Ermolenko <gnod94@gmail.com>
2017-04-22 15:01:38 +0600

    Create project

---
+++ Deboogger/Deboogger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Deboogger
+//
+//  Created by Nikita Ermolenko on 22/04/2017.
+//  Copyright © 2017 Nikita Ermolenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
